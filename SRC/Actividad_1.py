ddc3c40132a47299570b5ef66b8a91d77f0b0b8d
Federico <federico.montoyap@upb.edu.co>
2026-02-27 14:11:46 -0500

    1

---
+++ SRC/Actividad_1.py
@@ -0,0 +1,14 @@
+a = 34
+b = 45
+
+#a continuacion creo un condicional
+
+if a < b:
+    print ("b es mayor que a")
+else:
+    print ("a es mayor que b")
+  
+
+for p in range(100):
+    print("upb")
+    
