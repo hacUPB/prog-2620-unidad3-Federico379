ee8f6e93a415d3737ec822c182c52832ca84a43b
Federico <federico.montoyap@upb.edu.co>
2026-02-27 15:34:32 -0500

    2

---
+++ SRC/ejercicio1_aritmetica.py
@@ -0,0 +1,11 @@
+costo = 85
+
+
+propina = costo * 0.15
+total_propina = propina * 4
+total_cuenta = costo + total_propina
+individual = total_cuenta / 4
+
+print("Propina:", total_propina)
+print("cuenta con propina:", total_cuenta)
+print("valor por persona:", individual)
